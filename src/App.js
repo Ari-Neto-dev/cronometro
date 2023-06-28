c0f09c605b72539f60e7890ac502d76d5141f7e1
Ari Azevedo <azevedo.consultor@hotmail.com>
2023-06-28 12:39:56 -0300

    criando as classes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div className='container'>
+        <img src={require('./assets/cronometro.png')} alt="" className='img'/>
+        <a className='timer'> 0.0</a>
+
+        <div className='areaBotao'>
+          <a className='botao'> VAI </a>
+          <a className='botao'> LIMPAR </a>
+
+        </div>
+        
+      </div>
+    );
+  }
 }
-
 export default App;
