557fff2b46b2a2b403964f40688490c139a18557
Ari Azevedo <azevedo.consultor@hotmail.com>
2023-06-28 13:28:30 -0300

    estilizando

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import './style.css'
 
 class App extends Component{
   render(){
@@ -12,7 +13,7 @@ class App extends Component{
           <a className='botao'> LIMPAR </a>
 
         </div>
-        
+
       </div>
     );
   }
